aeeb0ef1e9269f1ca4a647bcfcb4f2cfb5a08b37
Aleksi Peebles <aleksi@iki.fi>
2016-11-13 15:32:17 +0200

    Refactor everything

--- dist/_twig-components/filters/t.filter.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$filter = new Twig_SimpleFilter('t', function ($string) {
-  return $string;
-});
--- dist/_twig-components/functions/path.function.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$function = new Twig_SimpleFunction('path', function ($string) {
-  return $string;
-});
