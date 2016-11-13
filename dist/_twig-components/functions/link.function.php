95831c81d27fd00bd50df0e9337b2fe58144cb3f
Aleksi Peebles <aleksi@iki.fi>
2016-11-13 20:00:29 +0200

    Remove last Drupal TwigExtension dependencies

--- dist/_twig-components/functions/link.function.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$function = new Twig_SimpleFunction(
-    'link',
-    function ($title, $url) {
-        return '<a href="' . $url . '">' . $title . '</a>';
-    },
-    array('is_safe' => array('html'))
-);
